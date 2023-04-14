4078780c5b47d2a4c23c169c46ba8f3db74d293a
Marco Forgione <marco.forgione1986@gmail.com>
2023-04-14 10:42:44 +0000

    transformer GPU setup

--- main_transformer.py
+++ main_transformer.py
@@ -1,60 +1,101 @@
+from pathlib import Path
 import time
 import torch
 import numpy as np
 from dataset import LinearDynamicalDataset
 from torch.utils.data import DataLoader
 from model_ts import GPTConfig, GPT
+import tqdm
 import argparse
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    # arguments
+    # Overall settings
+    out_dir = "out"
 
-    nx = 3
+    # System settings
+    nx = 10
     nu = 1
     ny = 1
-    seq_len = 100
-    max_iter = 10_000
+    seq_len = 500
 
+    # Transformer settings
     block_size = seq_len
-    n_layer = 4
+    n_layer = 8
     n_head = 4
     n_embd = 128
     dropout = 0.0
     bias = False
 
+    # Optimization settings
     weight_decay = 1e-1
-    learning_rate = 6e-4  # max learning rate
+    learning_rate = 1e-4
     beta1 = 0.9
     beta2 = 0.95
+    max_iter = 100_000
 
+
+    # Compute settings
+    cuda_device = "cuda:0"
+    no_cuda = False
+    threads = 5
+    compile = True
+
+    # Create out dir
+    out_dir = Path(out_dir)
+    out_dir.mkdir(exist_ok=True)
+
+    # Configure compute
+    torch.set_num_threads(threads)
+    use_cuda = not no_cuda and torch.cuda.is_available()
+    device_name  = cuda_device if use_cuda else "cpu"
+    device = torch.device(device_name)
+    device_type = 'cuda' if 'cuda' in device_name else 'cpu' # for later use in torch.autocast
+    torch.set_float32_matmul_precision("high")
+    #torch.backends.cuda.matmul.allow_tf32 = True # allow tf32 on matmul
+    #torch.backends.cudnn.allow_tf32 = True # allow tf32 on cudnn
+
+    # Create data loader
     train_ds = LinearDynamicalDataset(nx=nx, nu=nu, ny=ny, seq_len=seq_len)
-    train_dl = DataLoader(train_ds, batch_size=32, num_workers=4)
+    train_dl = DataLoader(train_ds, batch_size=32, num_workers=threads)
 
     model_args = dict(n_layer=n_layer, n_head=n_head, n_embd=n_embd, n_y=1, n_u=1, block_size=block_size,
-                      bias=bias, vocab_size=200, dropout=dropout)  # start with model_args from command line
+                      bias=bias, dropout=dropout)  # start with model_args from command line
     gptconf = GPTConfig(**model_args)
-    model = GPT(gptconf)
-    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4)
-    #optimizer = model.configure_optimizers(weight_decay, learning_rate, (beta1, beta2), device_type)
+    model = GPT(gptconf).to(device)
+    if compile:
+        model = torch.compile(model)  # requires PyTorch 2.0
 
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+    # Training loop
     time_start = time.time()
     LOSS = []
-    for itr, (batch_y, batch_u) in enumerate(train_dl):
-        #pass  # do something instead...
+    for itr, (batch_y, batch_u) in tqdm.tqdm(enumerate(train_dl)):
+
         optimizer.zero_grad()
+        batch_y = batch_y.pin_memory().to(device, non_blocking=True)
+        batch_u = batch_u.pin_memory().to(device, non_blocking=True)
         batch_y_pred, loss = model(batch_u, batch_y)
-        loss.backward()
         LOSS.append(loss.item())
-        print(f"{itr=} {loss=:.2f}")
+        if itr % 100 == 0:
+            print(f"\n{itr=} {loss=:.2f}\n")
         if itr == max_iter-1:
             break
+        loss.backward()
         optimizer.step()
 
     time_loop = time.time() - time_start
     print(f"\n{time_loop=:.2f} seconds.")
 
-    #batch_yu = torch.cat((batch_y, batch_u), dim=-1)
+    # Save results
+    checkpoint = {
+        'model': model.state_dict(),
+        'optimizer': optimizer.state_dict(),
+        'model_args': model_args,
+    }
+    torch.save(checkpoint, out_dir/"ckpt.pt")
+    
 
 
--- model_ts.py
+++ model_ts.py
@@ -122,7 +122,6 @@ class Block(nn.Module):
 @dataclass
 class GPTConfig:
     block_size: int = 1024
-    vocab_size: int = 50304  # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
     n_layer: int = 12
     n_head: int = 12
     n_embd: int = 768
@@ -136,7 +135,6 @@ class GPT(nn.Module):
 
     def __init__(self, config):
         super().__init__()
-        assert config.vocab_size is not None
         assert config.block_size is not None
         self.config = config
 
@@ -218,63 +216,6 @@ class GPT(nn.Module):
         self.transformer.wpe.weight = nn.Parameter(self.transformer.wpe.weight[:block_size])
         for block in self.transformer.h:
             block.attn.bias = block.attn.bias[:, :, :block_size, :block_size]
-
-    @classmethod
-    def from_pretrained(cls, model_type, override_args=None):
-        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
-        override_args = override_args or {}  # default to empty dict
-        # only dropout can be overridden see more notes below
-        assert all(k == 'dropout' for k in override_args)
-        from transformers import GPT2LMHeadModel
-        print("loading weights from pretrained gpt: %s" % model_type)
-
-        # n_layer, n_head and n_embd are determined from model_type
-        config_args = {
-            'gpt2': dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
-            'gpt2-medium': dict(n_layer=24, n_head=16, n_embd=1024),  # 350M params
-            'gpt2-large': dict(n_layer=36, n_head=20, n_embd=1280),  # 774M params
-            'gpt2-xl': dict(n_layer=48, n_head=25, n_embd=1600),  # 1558M params
-        }[model_type]
-        print("forcing vocab_size=50257, block_size=1024, bias=True")
-        config_args['vocab_size'] = 50257  # always 50257 for GPT model checkpoints
-        config_args['block_size'] = 1024  # always 1024 for GPT model checkpoints
-        config_args['bias'] = True  # always True for GPT model checkpoints
-        # we can override the dropout rate, if desired
-        if 'dropout' in override_args:
-            print(f"overriding dropout rate to {override_args['dropout']}")
-            config_args['dropout'] = override_args['dropout']
-        # create a from-scratch initialized minGPT model
-        config = GPTConfig(**config_args)
-        model = GPT(config)
-        sd = model.state_dict()
-        sd_keys = sd.keys()
-        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')]  # discard this mask / buffer, not a param
-
-        # init a huggingface/transformers model
-        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
-        sd_hf = model_hf.state_dict()
-
-        # copy while ensuring all of the parameters are aligned and match in names and shapes
-        sd_keys_hf = sd_hf.keys()
-        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')]  # ignore these, just a buffer
-        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')]  # same, just the mask (buffer)
-        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
-        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
-        # this means that we have to transpose these weights when we import them
-        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
-        for k in sd_keys_hf:
-            if any(k.endswith(w) for w in transposed):
-                # special treatment for the Conv1D weights we need to transpose
-                assert sd_hf[k].shape[::-1] == sd[k].shape
-                with torch.no_grad():
-                    sd[k].copy_(sd_hf[k].t())
-            else:
-                # vanilla copy over the other parameters
-                assert sd_hf[k].shape == sd[k].shape
-                with torch.no_grad():
-                    sd[k].copy_(sd_hf[k])
-
-        return model
 
     def configure_optimizers(self, weight_decay, learning_rate, betas, device_type):
         """
