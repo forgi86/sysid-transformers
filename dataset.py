c43efa336aaa4a380cf10e74954382f9040c4366
Marco Forgione <marco.forgione1986@gmail.com>
2023-04-23 15:52:04 +0000

    with wh

--- dataset.py
+++ dataset.py
@@ -32,40 +32,6 @@ class LinearDynamicalDataset(IterableDataset):
             yield torch.tensor(y), torch.tensor(u)
 
 
-import torch
-import numpy as np
-from torch.utils.data import DataLoader, IterableDataset
-import control  # pip install python-control, pip install slycot (optional)
-
-
-class LinearDynamicalDataset(IterableDataset):
-    def __init__(self, nx=5, nu=1, ny=1, seq_len=500, strictly_proper=True, dtype="float32", normalize=True):
-        super(LinearDynamicalDataset).__init__()
-        self.nx = nx
-        self.nu = nu
-        self.ny = ny
-        self.seq_len = seq_len
-        self.strictly_proper = strictly_proper
-        self.dtype = dtype
-        self.normalize = normalize
-
-    def __iter__(self):
-        while True:  # infinite dataset
-            # for _ in range(1000):
-            sys = control.drss(states=self.nx,
-                               inputs=self.nu,
-                               outputs=self.ny,
-                               strictly_proper=self.strictly_proper)
-            u = np.random.randn(self.nu, self.seq_len).astype(self.dtype)  # C, T as python-control wants
-            y = control.forced_response(sys, T=None, U=u, X0=0.0)
-            u = u.transpose()  # T, C
-            y = y.y.transpose().astype(self.dtype)  # T, C
-            if self.normalize:
-                y = (y - y.mean(axis=0)) / (y.std(axis=0))
-
-            yield torch.tensor(y), torch.tensor(u)
-
-
 class WHDataset(IterableDataset):
     def __init__(self, nx=5, nu=1, ny=1, seq_len=500, strictly_proper=True, dtype="float32", normalize=True):
         super(WHDataset).__init__()
@@ -79,6 +45,7 @@ class WHDataset(IterableDataset):
 
     def __iter__(self):
 
+        # A simple ff neural network
         def nn_fun(x):
             out = x @ w1.transpose() + b1
             out = np.tanh(out)
@@ -110,19 +77,19 @@ class WHDataset(IterableDataset):
             u = np.random.randn(self.seq_len, self.nu).astype(self.dtype)  # C, T as python-control wants
 
             # G1
-            x = control.forced_response(G1, T=None, U=u.transpose(), X0=0.0)
-            x = x.y.astype(self.dtype).transpose()  # T, C
-            x = (x - x.mean(axis=0)) / (x.std(axis=0))
+            y = control.forced_response(G1, T=None, U=u.transpose(), X0=0.0)
+            y = y.y.astype(self.dtype).transpose()  # T, C
+            y = (y - y.mean(axis=0)) / (y.std(axis=0) + 1e-6)
 
             # F
-            x = nn_fun(x)
+            y = nn_fun(y)
 
             # G2
-            x = control.forced_response(G2, T=None, U=x.transpose(), X0=0.0)
-            y = x.y.astype(self.dtype).transpose()
+            y = control.forced_response(G2, T=None, U=y.transpose(), X0=0.0)
+            y = y.y.astype(self.dtype).transpose()
 
             if self.normalize:
-                y = (y - y.mean(axis=0)) / (y.std(axis=0))
+                y = (y - y.mean(axis=0)) / (y.std(axis=0) + 1e-6)
 
             yield torch.tensor(y), torch.tensor(u)
 
