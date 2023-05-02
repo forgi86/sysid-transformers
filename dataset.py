4b8cd381ff35bc04c308f85b9ec41ac68fa3846b
forgi86 <marco.forgione1986@gmail.com>
2023-05-02 18:39:13 +0200

    Added fast lti simulation and smoother liner systems

--- dataset.py
+++ dataset.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 from torch.utils.data import DataLoader, IterableDataset
 import control  # pip install python-control, pip install slycot (optional)
+from lti import drss_matrices, dlsim
 
 
 class LinearDynamicalDataset(IterableDataset):
@@ -17,7 +18,7 @@ class LinearDynamicalDataset(IterableDataset):
 
     def __iter__(self):
         while True:  # infinite dataset
-        #for _ in range(1000):
+            # for _ in range(1000):
             sys = control.drss(states=self.nx,
                                inputs=self.nu,
                                outputs=self.ny,
@@ -27,7 +28,7 @@ class LinearDynamicalDataset(IterableDataset):
             u = u.transpose()  # T, C
             y = y.y.transpose().astype(self.dtype)  # T, C
             if self.normalize:
-                y = (y - y.mean(axis=0))/(y.std(axis=0) + 1e-6)
+                y = (y - y.mean(axis=0)) / (y.std(axis=0) + 1e-6)
 
             yield torch.tensor(y), torch.tensor(u)
 
@@ -94,9 +95,70 @@ class WHDataset(IterableDataset):
             yield torch.tensor(y), torch.tensor(u)
 
 
+class WHSmoothDataset(IterableDataset):
+    def __init__(self, nx=5, seq_len=500, strictly_proper=True, dtype="float32", normalize=True):
+        super(WHSmoothDataset).__init__()
+        self.nx = nx
+        self.seq_len = seq_len
+        self.strictly_proper = strictly_proper
+        self.dtype = dtype
+        self.normalize = normalize
+        self.strictly_proper = strictly_proper
+
+    def __iter__(self):
+
+        # A simple ff neural network
+        def nn_fun(x):
+            out = x @ w1.transpose() + b1
+            out = np.tanh(out)
+            out = out @ w2.transpose() + b2
+            return out
+
+        while True:  # infinite dataset
+            # for _ in range(1000):
+
+            n_in = 1
+            n_out = 1
+            n_hidden = 32
+
+            w1 = np.random.randn(n_hidden, n_in) / np.sqrt(n_in) * 5 / 3
+            b1 = np.random.randn(1, n_hidden) * 1.0
+            w2 = np.random.randn(n_out, n_hidden) / np.sqrt(n_hidden)
+            b2 = np.random.randn(1, n_out) * 1.0
+
+            G1 = drss_matrices(states=self.nx,
+                               inputs=1,
+                               outputs=1,
+                               strictly_proper=self.strictly_proper)
+
+            G2 = drss_matrices(states=self.nx,
+                               inputs=1,
+                               outputs=1,
+                               strictly_proper=False)
+
+            u = np.random.randn(self.seq_len, 1)  # C, T as python-control wants
+
+            # G1
+            y = dlsim(*G1, u)
+            y = (y - y.mean(axis=0)) / (y.std(axis=0) + 1e-6)
+
+            # F
+            y = nn_fun(y)
+
+            # G2
+            y = dlsim(*G2, y)
+
+            if self.normalize:
+                y = (y - y.mean(axis=0)) / (y.std(axis=0) + 1e-6)
+
+            u = u.astype(self.dtype)
+            y = y.astype(self.dtype)
+            yield torch.tensor(y), torch.tensor(u)
+
+
 if __name__ == "__main__":
-    train_ds = WHDataset(nx=5, nu=1, ny=1, seq_len=1000)
-    #train_ds = LinearDynamicalDataset(nx=5, nu=2, ny=3, seq_len=1000)
+    train_ds = WHSmoothDataset(nx=5, seq_len=1000)
+    # train_ds = LinearDynamicalDataset(nx=5, nu=2, ny=3, seq_len=1000)
     train_dl = DataLoader(train_ds, batch_size=32)
     batch_y, batch_u = next(iter(train_dl))
     print(batch_u.shape, batch_u.shape)
