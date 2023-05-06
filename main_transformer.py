d0b25cd66e78909e2412abb4bfb479984c454b27
forgi86 <marco.forgione1986@gmail.com>
2023-05-06 10:29:12 +0200

    small refactoring

--- main_transformer.py
+++ main_transformer.py
@@ -107,7 +107,7 @@ if __name__ == '__main__':
         # fix the keys of the state dictionary :(
         # honestly no idea how checkpoints sometimes get this prefix, have to debug more
         unwanted_prefix = '_orig_mod.'
-        for k,v in list(state_dict.items()):
+        for k, v in list(state_dict.items()):
             if k.startswith(unwanted_prefix):
                 state_dict[k[len(unwanted_prefix):]] = state_dict.pop(k)
         model.load_state_dict(state_dict)
