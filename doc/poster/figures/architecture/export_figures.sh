790b43da259e6ce3a2ffb451546e9e35a024b9eb
Marco Forgione <marco.forgione1986@gmail.com>
2024-04-19 10:39:25 +0200

    - added poster files

---
+++ doc/poster/figures/architecture/export_figures.sh
@@ -0,0 +1,3 @@
+alias inkscape="/Applications/Inkscape.app/Contents/MacOS/inkscape"
+inkscape *.svg  --export-area-drawing --export-type="pdf"
+inkscape *.svg  --export-area-drawing --export-dpi=400 --export-type="png"
